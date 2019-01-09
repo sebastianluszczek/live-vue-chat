691c51bbb6b7d1b7df157851351c14eaa1c8c73a
Sebastian <luszczek.sebastian@awesome-ind.com>
2019-01-09 09:53:57 +0100

    + chat mechanics and firebase connection

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -1,5 +1,5 @@
-import firebase from 'firebase';
-import firestorm from 'firebase/firestorm';
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
 
 // Initialize Firebase
 var config = {
